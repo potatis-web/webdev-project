ac34756c5a2b352eec8e91e2566204501ce6e6f0
potatis-web <elijir@edu.umea.se>
2026-03-31 10:45:00 +0200

    Refactor layout and styling in quiz component; update user data handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,18 +38,41 @@
       userData = JSON.parse(storedUser)
     }
   })
+  $effect(() => {
+    localStorage.setItem("user", JSON.stringify(userData))
+  })
 
 </script>
-
-<main>
-  {#if userData.ql}
-    {#each userData.ql as quiz (quiz.id)}
-      <div class="bg-amber-300 p-8">{quiz.name}</div>
-    {/each}
-  {/if}
-</main>
+<div class="w-screen h-screen grid grid-rows-[1fr_1fr_9fr] grid-cols-[1fr_9fr] gap-1">
+  <div class="hl border-b-0 flex justify-center items-center h-full">
+    <h1 class="text-blue-50">Quizmaker.gg</h1>
+  </div>
+  <nav class="hl"></nav>
+  <div class="hl border-t-0"></div>
+  <div class="hl flex itmes-center p-4">
+    <button class="text-blue-50 hover:bg-zinc-700/10 cursor-pointer">+ Add quiz</button>
+  </div>
+  <aside class="hl"></aside>
+  <main class=" hl flex flex-wrap gap-1 p-4">
+    {#if userData.ql}
+      {#each userData.ql as quiz (quiz.id)}
+        <div class="text-blue-50 p-4 h-48 w-1/5 hl">
+          {quiz.name}
+            <br>
+          {quiz.q.length + " questions"}</div>
+      {/each}
+    {/if}
+  </main>
+</div>
 <style>
+  @layer components {
+    .hl {
+      border: 1px;
+      border-style: solid;
+      border-color: var(--color-blue-50);
+    }
+  }
   :global(body) {
-    background-color: var(--color-gray-800);
+    background-color: var(--color-zinc-700);
   }
 </style>
